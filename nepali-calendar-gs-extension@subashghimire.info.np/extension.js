e59e4d6eaaf045fd8a911a611e01f18117701686
publishername <62213479+PublisherName@users.noreply.github.com>
2024-10-31 21:19:57 +0545

    Date Convertor Implemented.
    - Data added from 2081-2085 BS
    - Refactor CSS color In Calendar design

--- nepali-calendar-gs-extension@subashghimire.info.np/extension.js
+++ nepali-calendar-gs-extension@subashghimire.info.np/extension.js
@@ -7,14 +7,13 @@ import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
+import { formatExtractData, getNepaliDate } from "./utils/NepaliDateConverter.js"
+
 const Indicator = GObject.registerClass(
     class Indicator extends PanelMenu.Button {
         _init() {
             super._init(0.0, 'Nepali Date Extension');
-
             this._timeout = null;
-
-            // Container for top bar display (Nepali Date and Day of the Week)
             this._box = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
             this._nepaliDateLabel = new St.Label({
                 text: '',
@@ -23,28 +22,17 @@ const Indicator = GObject.registerClass(
             });
             this._box.add_child(this._nepaliDateLabel);
             this.add_child(this._box);
-
-            // Initialize the popup menu
             this._createPopupMenu();
-
-            // Initial update for date and time
             this._updateLabel();
-
-            // Add to GNOME Shell panel
             Main.panel.addToStatusArea('nepali-date-extension', this);
         }
 
         _createPopupMenu() {
-            // Create custom container for popup with vertical layout
             let popupContainer = new St.BoxLayout({
                 vertical: true,
                 style_class: 'custom-popup-container'
             });
-
-            // Year and Month Label
             this._yearMonthLabel = new St.Label({ text: '', style_class: 'year-month-label' });
-
-            // Day and Date in Circle
             let dayDateContainer = new St.BoxLayout({
                 style_class: 'day-date-circle',
                 vertical: true,
@@ -54,42 +42,33 @@ const Indicator = GObject.registerClass(
             this._dateLabel = new St.Label({ text: '', style_class: 'date-label' });
             dayDateContainer.add_child(this._dayLabel);
             dayDateContainer.add_child(this._dateLabel);
-
-            // English Date Label
             this._englishDateLabel = new St.Label({ text: '', style_class: 'english-date-label' });
-
-            // Tithi and Event Labels
             this._tithiLabel = new St.Label({ text: '', style_class: 'tithi-label' });
             this._eventLabel = new St.Label({ text: '', style_class: 'event-label' });
 
-            // Adding elements to popup container in order
             popupContainer.add_child(this._yearMonthLabel);
             popupContainer.add_child(dayDateContainer);
             popupContainer.add_child(this._englishDateLabel);
             popupContainer.add_child(this._tithiLabel);
             popupContainer.add_child(this._eventLabel);
 
-            // Wrap container in PopupBaseMenuItem and add to menu
             let customItem = new PopupMenu.PopupBaseMenuItem({ reactive: false });
             customItem.actor.add_child(popupContainer);
             this.menu.addMenuItem(customItem);
         }
 
         _updateLabel() {
-            let nepaliDateInfo = this._getNepaliDate();
+            let nepaliDateInfo = formatExtractData(getNepaliDate());
 
-            // Update top bar label with Nepali Date and Day of the Week
-            this._nepaliDateLabel.set_text(`${nepaliDateInfo.nepaliDate}  ${nepaliDateInfo.day} (${nepaliDateInfo.dayOfWeek})`);
+            this._nepaliDateLabel.set_text(`${nepaliDateInfo.npDay} ${nepaliDateInfo.npMonth} (${nepaliDateInfo.npDayName})`);
 
-            // Update popup menu content
-            this._yearMonthLabel.set_text(nepaliDateInfo.nepaliDate);
-            this._dayLabel.set_text(nepaliDateInfo.dayOfWeek);
-            this._dateLabel.set_text(nepaliDateInfo.day);
-            this._englishDateLabel.set_text(nepaliDateInfo.englishDate);
-            this._tithiLabel.set_text(nepaliDateInfo.tithi);
-            this._eventLabel.set_text(nepaliDateInfo.event);
+            this._yearMonthLabel.set_text(`${nepaliDateInfo.npYear} ${nepaliDateInfo.npMonth}`);
+            this._dayLabel.set_text(nepaliDateInfo.npDayName);
+            this._dateLabel.set_text(nepaliDateInfo.npDay);
+            this._englishDateLabel.set_text(nepaliDateInfo.enDate);
+            this._tithiLabel.set_text(nepaliDateInfo.npTithi);
+            this._eventLabel.set_text(nepaliDateInfo.npEvent.split('/').join('\n'));
 
-            // Update label every hour
             if (this._timeout) GLib.source_remove(this._timeout);
             this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 3600, () => {
                 this._updateLabel();
@@ -97,26 +76,11 @@ const Indicator = GObject.registerClass(
             });
         }
 
-        _getNepaliDate() {
-            // Replace with real API call
-            return {
-                nepaliDate: "२०८१ कार्तिक",
-                dayOfWeek: "शुक्रवार",
-                day: "२३",
-                englishDate: "Nov 08, 2024",
-                tithi: "कार्तिक शुक्ल सप्तमी",
-                event: "विश्व रेडियोग्राफी दिवस (World Radiography Day)"
-            };
-        }
-
         destroy() {
-            // Remove timeout if it exists
             if (this._timeout) {
                 GLib.source_remove(this._timeout);
                 this._timeout = null;
             }
-
-            // Destroy the Indicator
             super.destroy();
         }
     });
---
+++ nepali-calendar-gs-extension@subashghimire.info.np/utils/NepaliDateConverter.js
@@ -0,0 +1,130 @@
+import GLib from 'gi://GLib';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+const Me = Main.extensionManager.lookup('nepali-calendar-gs-extension@subashghimire.info.np');
+
+export const readJSONFile = (filePath) => {
+    try {
+        const fullPath = `${Me.path}/${filePath}`;
+        let fileContent = String(GLib.file_get_contents(fullPath)[1]);
+        return JSON.parse(fileContent);
+    } catch (e) {
+        log(`Error reading JSON file: ${filePath}`);
+        return {};
+    }
+}
+
+export const getNepaliDate = () => {
+    const nepcal = {
+        81: {
+            mon_days: [31, 32, 31, 32, 31, 30, 30, 30, 29, 30, 29, 31],
+            tot_days: 366,
+        },
+        82: {
+            mon_days: [30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30],
+            tot_days: 365,
+        },
+        83: {
+            mon_days: [31, 31, 32, 31, 31, 30, 30, 30, 29, 30, 30, 30],
+            tot_days: 365,
+        },
+        84: {
+            mon_days: [31, 31, 32, 31, 31, 30, 30, 30, 29, 30, 30, 30],
+            tot_days: 365,
+        },
+        85: {
+            mon_days: [31, 32, 31, 32, 30, 31, 30, 30, 29, 30, 30, 30],
+            tot_days: 365,
+        }
+    };
+
+    const ref_date_nep = [2081, 1, 1];
+    const ref_date_eng = new Date(2024, 3, 13);
+    const inp_date_eng = new Date();
+
+    let day_diff = Math.floor((inp_date_eng - ref_date_eng) / (1000 * 3600 * 24));
+    let month_diff = 0;
+    let year_diff = 0;
+    let stop_loop = false;
+    let start_year = '81';
+
+    while (!stop_loop) {
+        if (day_diff > nepcal[start_year]['tot_days']) {
+            year_diff++;
+            day_diff -= nepcal[start_year]['tot_days'];
+        } else {
+            for (let i = 0; i < nepcal[start_year]['mon_days'].length; i++) {
+                let days_mon = nepcal[start_year]['mon_days'][i];
+                if (day_diff >= days_mon) {
+                    month_diff++;
+                    day_diff -= days_mon;
+                } else {
+                    stop_loop = true;
+                    break;
+                }
+            }
+        }
+        if (!stop_loop) {
+            start_year++;
+        }
+    }
+
+    let date_diff = [year_diff, month_diff, day_diff];
+
+    for (let i = 0; i < ref_date_nep.length; i++) {
+        date_diff[i] += ref_date_nep[i];
+        if (i == 1) {
+            if (date_diff[i] >= 13) {
+                date_diff[i] -= 12;
+                date_diff[i - 1] += 1;
+            }
+            var month = date_diff[i];
+        }
+    }
+
+    return {
+        npYear: date_diff[0],
+        npMonth: month,
+        npDay: date_diff[2],
+        npDayName: inp_date_eng.getDay() + 1,
+        enDate: inp_date_eng.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: '2-digit' })
+    }
+}
+
+
+
+export const formatExtractData = (nepaliDate) => {
+    const months = [
+        'बैशाख', 'जेठ', 'असार', 'साउन', 'भदौ', 'असोज', 'कार्तिक', 'मंसिर', 'पुष', 'माघ', 'फाल्गुन', 'चैत'
+    ];
+
+    const daysOfWeek = ["आइतबार", "सोमबार", "मंगलबार", "बुधबार", "बिहीबार", "शुक्रबार", "शनिबार"];
+
+    const year = nepaliDate.npYear.toString().split('').map(num => '०१२३४५६७८९'[num]).join('');;
+    const month = months[nepaliDate.npMonth - 1];
+    const day = nepaliDate.npDay.toString().split('').map(num => '०१२३४५६७८९'[num]).join('');;
+    const dayName = daysOfWeek[nepaliDate.npDayName - 1];
+
+    const data = readJSONFile(`data/${nepaliDate.npYear}.json`);
+    let tithi = '-';
+    let event = '-';
+
+    if (data[nepaliDate.npMonth - 1]) {
+        const days = data[nepaliDate.npMonth - 1].days;
+        const dayData = days.find(d => d.dayInEn === nepaliDate.npDay.toString());
+        if (dayData) {
+            tithi = dayData.tithi || '-';
+            event = dayData.event || '-';
+        }
+    }
+
+    return {
+        npYear: year,
+        npMonth: month,
+        npDay: day,
+        npDayName: dayName,
+        enDate: nepaliDate.enDate,
+        npTithi: tithi,
+        npEvent: event
+    };
+}
