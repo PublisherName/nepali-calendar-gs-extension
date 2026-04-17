510b0f0207b35ccd454de176fa8ddf88c23457fc
Subash Ghimire <62213479+PublisherName@users.noreply.github.com>
2026-04-17 23:42:13 +0545

    chore(shexli): add shexli dependency and update relase workflow (#19)

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 
 cd nepali-calendar-gs-extension@subashghimire.info.np
 
-glib-compile-schemas ../nepali-calendar-gs-extension@subashghimire.info.np/schemas
-
 zip -r ../nepali-calendar-gs-extension@subashghimire.info.np.zip *.js metadata.json stylesheet.css data/*.json utils/*.js schemas/*
